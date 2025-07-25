8914358423d45c969ff2ae5722e396b18622ac7f
grantula <walter.grant.ward@gmail.com>
2025-07-25 11:31:58 -0400

    ;

--- generate_html.sh
+++ generate_html.sh
@@ -3,11 +3,13 @@ cp $1 ~/pdf
 filename=$(basename "$1")
 html_filename="${filename%.*}.html"
 renamed_index="resume-$(date +%F).html"
-echo $filename
-echo $html_filename
+
+echo "Filename: $filename"
+echo "HTML Filename: $html_filename"
+echo "Renamed Index Filename: $renamed_index"
 
 # Replace special character with a placeholder
-sed -i -e 's/❖/!~!/g' ~/pdf/$filename
+# sed -i -e 's/❖/!~!/g' ~/pdf/$filename
 
 docker run \
     -ti \
@@ -20,12 +22,22 @@ docker run \
     /pdf/$filename \
     $html_filename
 
+# Copy the file from the docker container to the host
+docker cp $(docker ps -lq):/pdf/$html_filename ~/pdf/$html_filename
+
 # Replace placeholder with special character
-sed -i -e 's/!~!/❖/g' ~/pdf/$html_filename
+# sed -i -e 's/!~!/❖/g' ~/pdf/$html_filename
 
 cp ~/pdf/$html_filename .
 
-mv index.html $renamed_index
+if [ -f index.html ]; then
+    echo "index.html already exists, renaming it to $renamed_index"
+    mv index.html $renamed_index
+else
+    echo "index.html does not exist, creating a new one"
+    
+fi
+
 echo "Moving old file to $renamed_index"
 
 mv ./$html_filename index.html
